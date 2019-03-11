a848e0047dd3847bd93e5a901a6df3e06724f9f5
dancervero123 <43342097+dancervero123@users.noreply.github.com>
2019-03-11 08:22:55 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
-$(".qone").click(function(){
+$("button").click(function(){
     var em=$(".eom").val();
     $(".end").append(em);
-    });
-    
-$(".qtwo").click(function(){
     var tc=$(".toc").val();
     $(".nde").append(tc);
-    });
-          
-$(".qthree").click(function(){
     var year=$(".baby").val();
     $(".dne").append(2019 - parseInt(year));
-    });
-
-    
-$(".qfour").click(function(){
+    var color=$(".blue").val();
     var number=$(".ten").val();
     var year=$(".baby").val();
     $(".now").append((2019 - parseInt(year))*parseInt(number));
     $(".won").append(number);
-    });
-    
-    
-$(".qfive").click(function(){
-    var color=$(".blue").val();
     $(".ew").append(color);
     $("p").show();
     $("h1").hide();
@@ -33,7 +19,4 @@ $(".qfive").click(function(){
     $("input").hide();
     $(".other").hide();
     });
-    
-
-
 
